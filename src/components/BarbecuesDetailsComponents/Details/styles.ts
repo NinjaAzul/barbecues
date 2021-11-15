a896036fcd20d32bbb3de2753e8576164a23e6ef
NinjaAzul <erick@fasters.com.br>
2021-11-15 12:09:33 -0300

    update redme

--- src/components/BarbecuesDetailsComponents/Details/styles.ts
+++ src/components/BarbecuesDetailsComponents/Details/styles.ts
@@ -22,7 +22,7 @@ export const Content = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-justify-content: center;
+justify-content: flex-start;
 width: 100%;
 gap:2.688rem;
 height: 100vh;
